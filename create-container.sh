93f5e74d56f7ee1262944603c287acf90e3c6a6b
teruteruboze <akazawa-teruaki@ed.tmu.ac.jp>
2022-04-14 12:57:37 +0900

    rename scripts

--- create-container.sh
+++ create-container.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 source ./conf.txt
 # =================================================
-docker build . -t "$BUILD_IMAGE_NAME" -f Dockerfile --build-arg IMAGE="$BASE_IMAGE_NAME" --build-arg TAG="$BASE_IMAGE_TAG" --build-arg UID=`id -u`
+if test "$WORK_DIR" = "" ; then
+  WORK_DIR=`pwd`
+fi
+if test $USE_GPU -eq 1 ; then
+  USE_GPU="--gpus all"
+else
+  USE_GPU=""
+fi
+if test "$PORT" != "" ; then
+  PORT="-p "$PORT":8888"
+fi
+if test $BACKGROUND -eq 1 ; then
+  BACKGROUND="-d"
+else
+  BACKGROUND=""
+fi
+# =================================================
+CREATE_CONTAINER_COMMAND="docker run "$BACKGROUND" -it "$USE_GPU" "$PORT" -v "$WORK_DIR":/work --name "$CONTAINER_NAME" "$BUILD_IMAGE_NAME""
+$CREATE_CONTAINER_COMMAND
